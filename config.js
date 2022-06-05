62d865ef7c3c4acae9509a070751b140a3b38cc8
quang5203 <106916879+quang5203@users.noreply.github.com>
2022-06-05 20:52:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'thu hìn à!',
+    introTitle: 'Ánh à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Lạc vào tim mình lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là yeu anh <33',
+    title: 'Phải chăng mình đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng mình đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích cậu <33',
     btnNo: 'Không be oi :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'iu em lắm vợ iu à.',
+    question:'thích mình nhiều hay ít<3',
+    btnReply: 'Gửi cho Hoàng <3',
+    reply: 'thích nhiều lắm<33333333',
+    mess: 'mình biết mà 🥰. mình cũng vậy 😘😘',
+    messDesc: 'hihii.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
